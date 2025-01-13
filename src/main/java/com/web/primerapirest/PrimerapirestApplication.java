e1e94e38312610121aefd804ced1c7a2bf936fc4
Jarood <jaroodjimili@gmail.com>
2025-01-13 10:03:16 -0500

    iniciando proyecto

---
+++ src/main/java/com/web/primerapirest/PrimerapirestApplication.java
@@ -0,0 +1,14 @@
+package com.web.primerapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerapirestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("wewer");
+		SpringApplication.run(PrimerapirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/primerapirest/PrimerapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.primerapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
